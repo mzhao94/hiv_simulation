8b31e9f437ff0fba0c42dc3204b901946360ba62
mzhao94 <mzhao94@stanford.edu>
2018-10-04 16:39:33 -0500

    debugged the infection code so that it works with the mixing matrix input files

--- code/infection.m
+++ code/infection.m
@@ -2,51 +2,68 @@
 % matrix, and a mixing matrix and will transition people in this
 % demographic group to infection based on a uniform random number draw
 
-function updated_state_matrix = infection(reference_demog_group_def, demog_table, DemogTblCols, mixing_matrix, state_matrix, StateMatCols)
+function updated_state_matrix = infection(reference_demog_group_def, DemogTblCols, mixing_matrix, mixing_table, MixingTblCols, state_matrix, StateMatCols)
     
-    % Read in the betas for this particular demographic group (i.e. row in
-    % the matrix)
-    betas = get_mixing_matrix_row(mixing_matrix, reference_demog_group_def, DemogTblCols);
-    
+    % Returns the row index based on the mixing_mat_def
+    % demographic group table
+    demog_row = find_demog_rows(mixing_table, MixingTblCols, reference_demog_group_def, DemogTblCols, MixingTblCols);
+
+    % Based on the demographic row, extract the betas from the
+    % corresponding row number in the mixing matrix
+    betas = mixing_matrix(demog_row,:);
+        
     % Placeholder for adding up all the infection probabilities over all
     % demographic group our reference group is mixing with
     total_infec_prob = 0;
     
-    % ASSUMPTION: the demog table and the mixing matrix have
-    % the same order of demographic groups
     
+    %%%% NOTE: STOPPED HERE, NEED TO DEBUG calc_infec_prob because it's
+    %%%% giving NaN
+   
     % For each of all possible demographic groups
-    for demog_group_idx = 1:size(betas)
-        
+    for demog_group_idx = 1:size(betas, 2)
+
         % Get the demographic group definition we are mixing with
-        other_demog_group_def = demog_table(demog_group_idx,:);
-        
+        other_demog_group_def = mixing_table(demog_group_idx,:);
+
         % Calculate infection probability
         beta = betas(demog_group_idx);
-        infec_prob = beta * calc_infec_prob(state_matrix, StateMatCols, other_demog_group_def, DemogTblCols);
+        infec_prob = beta * calc_infec_prob(state_matrix, StateMatCols, other_demog_group_def, DemogTblCols, MixingTblCols);
         
         % Add this to the total infection probability over all demographic
         % groups we are mixing with
         total_infec_prob = total_infec_prob + infec_prob;
-        
     
+    end  
+
     % Find the people in the demographic group
-    reference_demog_mat_indices = find_demog_rows(state_matrix, StateMatCols, reference_demog_group_def, demog_col_struct);
+    reference_demog_mat_indices = find_demog_rows(state_matrix, StateMatCols, reference_demog_group_def, DemogTblCols, MixingTblCols);
     
     % Find eligible people to infect in our reference demographic group (i.e. no hiv, no prep, no art, alive)
-    eligible_rows = find_indices(state_matrix, reference_demog_mat_indices, 'alive', '=', 1);
-    eligible_rows = find_indices(state_matrix, eligible_rows, 'hiv', '=', 0);
-    eligible_rows = find_indices(state_matrix, eligible_rows, 'prep', '=', 0);
+    susceptible = find_indices(state_matrix, reference_demog_mat_indices, StateMatCols.alive, '=', 1);
+    susceptible = find_indices(state_matrix, susceptible, StateMatCols.hiv, '=', 0);
+    susceptible = find_indices(state_matrix, susceptible, StateMatCols.prep, '=', 0);
+    susceptible = find_indices(state_matrix, susceptible, StateMatCols.art, '=', 0);
+
+    disp('S')
+    disp(susceptible)
+    % (I/N) * S
+    total_infec_prob = total_infec_prob * length(susceptible);
     
     % Do a random number draw for all of the eligible rows
-    unif_prob = rand([1, length(eligible_rows)]);
+    unif_prob = rand([1, length(susceptible)]);
+        
+    disp(reference_demog_group_def)
+    disp(susceptible)
+    disp(total_infec_prob)
+    disp(unif_prob)
 
     % Compare to random numbers picked from a uniform
     % distribution and determine who is infected
     to_infect = total_infec_prob > unif_prob;
                         
     % Get indices to infect based on people who are eligible
-    state_mat_infec_rows = eligible_rows(to_infect);
+    state_mat_infec_rows = susceptible(to_infect);
     
     % Infect chosen people
     state_matrix(state_mat_infec_rows, StateMatCols.hiv) = 1;
@@ -54,5 +71,4 @@ function updated_state_matrix = infection(reference_demog_group_def, demog_table
     % Return the updated state matrix
     updated_state_matrix = state_matrix;
     
-    end
 end

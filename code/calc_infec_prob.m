2e0dbd17efc9eb210b0d486dfb71d2c5ba99f41a
mzhao94 <mzhao94@stanford.edu>
2018-10-04 16:36:38 -0500

    fixed the function so that the infection probability calculated is applied to the nubmer of susceptible people in our reference demographic group

--- code/calc_infec_prob.m
+++ code/calc_infec_prob.m
@@ -2,25 +2,34 @@
 % probabilities for each demographic group using the formula 
 % P(infection) = (I/N) * S
 
-function infection_probability = calc_infec_prob(state_matrix, StateMatCols, demog_group_def, demog_col_struct)
+function infection_probability = calc_infec_prob(state_matrix, StateMatCols, demog_group_def, demog_col_struct, common_col_struct)
 
     % Find the people in the demographic group
-    demog_mat_indices = find_demog_rows(state_matrix, StateMatCols, demog_group_def, demog_col_struct);
+    demog_mat_indices = find_demog_rows(state_matrix, StateMatCols, demog_group_def, demog_col_struct, common_col_struct);
     
     % find total number of people in the demographic group (N)
     N = size(demog_mat_indices, 2);
+    disp(demog_group_def)
+    disp('N')
+    disp(N)
+    
+    % If there are no people in the demographic group, return an infection
+    % probability of 0
+    if N == 0
+       infection_probability = 0;
+       return;
+    end
 
-    % calculate the healthy people in the demographic group (S)
-    healthy = find_indices(state_matrix, demog_mat_indices, StateMatCols.hiv, '=', 0);
-    S = size(healthy, 2);
-
-    % find all the transmitting people in the demogrphic group (I) -
+    % find all the transmitting people in the demographic group (I) -
     % defined by their hiv status of at least 1, and prep and art status of 0
     has_hiv = find_indices(state_matrix, demog_mat_indices, StateMatCols.hiv, '>', 0);
     has_hiv_no_art = find_indices(state_matrix, has_hiv, StateMatCols.art, '=', 0);
     has_hiv_no_art_no_prep = find_indices(state_matrix, has_hiv_no_art, StateMatCols.prep, '=', 0);
-    I = size(has_hiv_no_art_no_prep, 2);
     
-    infection_probability = (I/N) * S;
-
+    I = size(has_hiv_no_art_no_prep, 2);
+    disp('I')
+    disp(I)
+    
+    infection_probability = I/N;
+    
 end

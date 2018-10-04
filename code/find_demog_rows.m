cb55a546c9f94d4a5c5930c54440367dcab252c7
mzhao94 <mzhao94@stanford.edu>
2018-10-04 16:38:01 -0500

    modified parameters to take in another struct that specifies which fields to loop through in each the target table and the demographic table; might need to restructure this function but for now should work

--- code/find_demog_rows.m
+++ code/find_demog_rows.m
@@ -1,13 +1,14 @@
 % Get the row corresponding to the demographic group
-function demog_mat_indices = find_demog_rows(target_table, target_table_col_struct, demog_group_def, demog_col_struct)
+% The common_col_struct provides all the fields we want to match
+function demog_mat_indices = find_demog_rows(target_table, target_table_col_struct, demog_group_def, demog_col_struct, common_col_struct)
 
     % Account for the two types of age paradigms (age range with min/max,
     % and just age as an integer)
     
     demog_mat_indices = 1:size(target_table,1);
-
+    
     if isfield(target_table_col_struct, 'age')
-        for field = fieldnames(demog_col_struct).'
+        for field = fieldnames(common_col_struct).'
             field_string = string(field);
             if strcmp(field_string,'age_min')
                 demog_group_age_min = demog_group_def(demog_col_struct.age_min);
@@ -21,7 +22,7 @@ function demog_mat_indices = find_demog_rows(target_table, target_table_col_stru
             end
         end
     else
-        for field = fieldnames(demog_col_struct).'
+        for field = fieldnames(common_col_struct).'
             field_string = string(field);
             demog_field_value = demog_group_def(demog_col_struct.(field_string));
             demog_mat_indices = find_indices(target_table, demog_mat_indices, target_table_col_struct.(field_string), '=', demog_field_value); 

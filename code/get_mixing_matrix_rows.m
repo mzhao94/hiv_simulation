bfa52cbc6574337cf3cfc03bfed56c1599428462
mzhao94 <mzhao94@stanford.edu>
2018-10-12 15:24:02 -0700

    remove unnecessary function

--- code/get_mixing_matrix_rows.m
+++
@@ -1,7 +0,0 @@
-function mixing_matrix_row = get_mixing_matrix_rows(reference_demog_group_def, demog_table, DemogTblCols, mixing_matrix, state_matrix, StateMatCols)
-    
-    % Figure out which row to get based on the reference_demog_group_def
-    
-    % Return that row
-
-end
